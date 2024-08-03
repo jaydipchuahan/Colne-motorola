eab66aff6fb6be75a5620dcd46732cd1f419d313
jaydipchuahan <144215631+jaydipchuahan@users.noreply.github.com>
2024-08-04 00:23:26 +0530

    Update sript.js

--- sript.js
+++ sript.js
@@ -24,20 +24,59 @@ gsap.from(".Smartphones", {
     scrollTrigger: {
         trigger: ".Smartphones",
         scroller: "body",
-        start: "top 75%"
+        start: "top 75%",
     },
     y: 200,
     opcity: 0,
     duration: 1,
     stagger: 0.2
 })
+gsap.from(".ani-l", {
+    y: 500,
+    x: -10,
+    opcity: 0,
+    duration: 0.5,
+    stagger: 0.3,
+
+})
+var swiper = new Swiper(".mySwiper", {
+    rewind: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
 
 // menu-left-slider 
-function onhoverShowMenu() {
-    let MenuSlider = document.getElementById("menu-slide");
-    MenuSlider.classList.add(".add-anim");
-    MenuSlider.style.transform = "translate(0, 0)";
-}
-function menuclose() {
-    document.getElementById("menu-slide").style.transform = "translate(-100%, 0)";
-}
+let tl = gsap.timeline();
+tl.to("#menu-slide", {
+    left: 500,
+    duration: 0.32,
+    opacity: 1
+})
+tl.from("#menu-slide a", {
+    opacity: 0,
+    x: -300,
+    duration: 0.3,
+    stagger: 0.1
+
+})
+tl.from("#menu-slide i", {
+    x: 10,
+    opacity: 0,
+    duration: 0.2
+})
+tl.pause();
+
+let menu = document.querySelector(".menu");
+let menuClose= document.querySelector("#menu-slide i");
+menu.addEventListener("click", function () {
+    tl.play();
+})
+menuClose.addEventListener("click", function () {
+    tl.reverse();
+})
+
+
