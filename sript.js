fcd2d1d8e94d7e5f95858d5bec65e44a2dfd776d
jaydipchuahan <144215631+jaydipchuahan@users.noreply.github.com>
2024-08-05 17:13:38 +0530

    Update sript.js

--- sript.js
+++ sript.js
@@ -16,7 +16,7 @@ gsap.from(".Phone-Models", {
         start: "top 80%"
     },
     y: 200,
-    opcity: 0,
+    opacity: 0,
     duration: 0.5,
     stagger: 0.2
 })
@@ -27,14 +27,13 @@ gsap.from(".Smartphones", {
         start: "top 75%",
     },
     y: 200,
-    opcity: 0,
+    opacity: 0,
     duration: 1,
     stagger: 0.2
 })
 gsap.from(".ani-l", {
-    y: 500,
-    x: -10,
-    opcity: 0,
+    y: 40,
+    opacity: 0,
     duration: 0.5,
     stagger: 0.3,
 
@@ -71,7 +70,7 @@ tl.from("#menu-slide i", {
 tl.pause();
 
 let menu = document.querySelector(".menu");
-let menuClose= document.querySelector("#menu-slide i");
+let menuClose = document.querySelector("#menu-slide i");
 menu.addEventListener("click", function () {
     tl.play();
 })
@@ -79,4 +78,19 @@ menuClose.addEventListener("click", function () {
     tl.reverse();
 })
 
+let navIconeRouted=document.querySelector(".Header .left-side .nav .ul .list-items .list-link i");
+let navLi = document.querySelectorAll(".Header .left-side .nav .ul .list-items .list-link");
+let phoneList = document.querySelector(".phones-list");
 
+navLi[0].addEventListener("mouseenter", function () {
+    phoneList.style.display = "inline-flex";
+    phoneList.style.transition="1s ease-in";
+    navIconeRouted.style.transform="rotate(0deg)"
+    navIconeRouted.style.transition=".5s";
+})
+phoneList.addEventListener("mouseleave", function () {
+    phoneList.style.display = "none";
+    phoneList.style.transition=".5s";
+
+
+})
